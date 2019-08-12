1034ae65aff21a5c403855b57cb0d2f14b256ca3
Colin Williams <lackita@gmail.com>
2019-08-12 09:39:11 -0700

    Gem Skeleton
    Because I'm doing this on my phone, I didn't do a lot of the
    documentation I would have done otherwise. I plan on filling that in
    once I am able to work on my laptop.

---
+++ lib/ruby/refactor.rb
@@ -0,0 +1,8 @@
+require "ruby/refactor/version"
+
+module Ruby
+  module Refactor
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby/refactor/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Refactor
+    VERSION = "0.1.0"
+  end
+end
